1637394af6462ffefddc04eea833085e41169598
Grzegorz <GrzegoRes@gmail.com>
2022-07-14 13:41:26 +0200

    Add project files.

---
+++ Algorytm5/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> listOfWords = new List<string>() { "eat", "ate", "apt", "pat", "tea", "now" };
+
+var listOfGroupAnagram = GetAllAnagramsGroupFromList(listOfWords);
+
+foreach(var group in listOfGroupAnagram)
+{
+    foreach(var word in group)
+    {
+        Console.Write($"{word},");
+    }
+    Console.WriteLine();
+}
+
+List<IGrouping<string,string>> GetAllAnagramsGroupFromList(List<string> words)
+{
+    return words.GroupBy(word => word = string.Concat(word.OrderBy(ch => ch))).ToList();
+}
